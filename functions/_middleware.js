8e4b0195be17a26de51f4d8718d931de068b6aa1
Ndraawz <wolfykuns15@gmail.com>
2025-07-10 03:20:36 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url)
   if (url.pathname.endsWith('.lua')) {
-    return new Response('404Not Found', { status: 404 })
+    return new Response('404 Not Found', { status: 404 })
   }
 
   return context.next()
