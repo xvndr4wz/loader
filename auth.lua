59ca2247d224427eee0c4098a1aee4515848aff4
Ndraawz <wolfykuns15@gmail.com>
2025-07-10 04:42:21 +0700

    Update and rename auth.lua to auth

--- auth.lua
+++
@@ -1,9 +0,0 @@
-return function(id)
-    local url = "https://loader-5xf.pages.dev/" .. id
-    local success, response = pcall(game.HttpGet, game, url)
-    if success then
-        return loadstring(response)()
-    else
-        warn("Failed to load script:", id)
-    end
-end
