00721eb7ef0096ea8b0392108193b68dffd45e8c
Ndraawz <wolfykuns15@gmail.com>
2025-07-10 03:25:26 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,20 +1,8 @@
 export async function onRequest(context) {
-  const url = new URL(context.request.url)
-  if (url.pathname.endsWith('.lua')) {
-    return new Response(`
-      <!DOCTYPE html>
-      <html>
-      <head><title>404 Ga Ketemu</title></head>
-      <body style="background:black;color:lime;text-align:center;padding-top:100px;font-family:monospace">
-        <h1>404 - Scriptnya Ga Ada, Bocil!</h1>
-        <p>Mau nyolong ya? 😏<br>Gak semudah itu, Ferguso.</p>
-      </body>
-      </html>
-    `, {
-      status: 404,
-      headers: { 'content-type': 'text/html' }
-    })
-  }
+  const url = new URL(context.request.url)
+  if (url.pathname.endsWith('.lua')) {
+    return new Response('404 Not Found', { status: 404 })
+  }
 
-  return context.next()
+  return context.next()
 }
