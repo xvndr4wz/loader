f0818b8c57a3699a82bbdc65c6233ae6413a31db
xndr4wz <wolfykuns15@gmail.com>
2025-10-26 23:17:09 +0700

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -1,12 +1,24 @@
-local ok, err = pcall(function()
-    local s = game:HttpGet('https://ndraawzz-developer.vercel.app/auth.lua')
-    local f = loadstring(s)
-    if not f then error("compile error") end
-    f()
-end)
+function Ioad(name)
+    local sources = {
+        "https://loader-draaxz.pages.dev/",
+        "https://ndraawzz-developer.vercel.app/"
+    }
 
-if ok and type(Ioad) == "function" then
-    Ioad("Invisible")
-else
-    warn("Gagal auth atau Ioad tidak tersedia: "..tostring(err))
+    for _, baseUrl in ipairs(sources) do
+        local ok, err = pcall(function()
+            local response = game:HttpGet(baseUrl .. name .. ".lua")
+            loadstring(response)()
+        end)
+        if ok then
+            print("Loaded from: ".. baseUrl .. name .. ".lua")
+            return
+        else
+            warn("Gagal: ".. tostring(err))
+        end
+    end
+
+    warn("Gagal memuat script: ".. tostring(name))
 end
+
+-- panggil cukup dengan nama (tanpa leading slash atau .lua)
+Ioad("Invisible")
