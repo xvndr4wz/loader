b9ca1ba19ae9f0336747f081dd628739a57cd531
xndr4wz <wolfykuns15@gmail.com>
2025-10-31 14:52:24 +0700

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -6,13 +6,14 @@
     ██║ ╚████║██████╔╝██║  ██║██║  ██║██║  ██║╚███╔███╔╝███████╗
     ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚══╝╚══╝ ╚══════╝
     
-    ================================================================
-    ||  Developer  : Ndraawz                                      ||
-    ||  Obfuscator : Ndraawz                                      ||
-    ||  Status     : Protected & Encrypted                        ||
-    ||  Warning    : Do Not Steal or Redistribute                 ||
-    ================================================================
+    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+    👤 Developer  : Ndraawz
+    🔒 Obfuscator : Ndraawz
+    ⚡ Status     : Protected & Encrypted
+    ⚠️  Warning    : Do Not Steal or Redistribute
+    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ]]
+
 function Ioad(name)
     local baseUrl = "https://ndraawzz-developer.vercel.app/api/"
     local token = "HJDBSBCUJJRFMIK2ARAF2W3ZCMRF2C3BFUNWIHTFC4GGUR3MC4FUKUDMCMHGEBSUAEYV4ILKAYWFIH3DFIZQYSCKMJLBWHCSKIMAMYJFBQPQK6ZSEEFBOWR4GAQFM5ZFLNFB2DZEDQ7VSXYTAENBYQYTKVDQI52VM5IR2ULOIVDB6DCJ"
